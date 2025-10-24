ce5398f8da577be13a17191c152fe3f0ed3e163a
manyuyugong <3225016302@qq.com>
2025-10-24 20:46:10 +0800

    lab2 B init

--- symbol_table.h
+++ symbol_table.h
@@ -12,6 +12,31 @@ typedef struct Symbol {
     int   kind; /* e.g., VAR/FUNC/STRUCT */
 } Symbol;
 
+typedef struct FieldList_* FieldList;
+typedef struct Type_* Type;
+
+struct FieldList_{
+	char name[32];//域的名字;和树节点名字长度一样;
+	Type type;//域的类型;
+	FieldList tail;//下一个域
+};
+
+struct Type_{
+	enum { BASIC=0,ARRAY=1,STRUCTURE=2,FUNCTION=3,STRUCT_NAME_=4} kind;
+	union{
+		//基本类型
+		int basic;//0表示int,1表示float;
+		//数组类型信息包括元素类型与数组大小构成
+		struct{ Type elem;int size;}array_;
+		//结构体类型的信息是一个链表
+		struct{FieldList structure;char *name;}structure_;
+		struct{
+			Type returnparam;//返回的参数类型
+			int paramnums;//输入的参数数量
+			FieldList params;//函数的具体参数;
+			}function;
+}u;
+};
 /* Lifecycle */
 void initSymbolTable(void);
 void freeSymbolTable(void);
@@ -29,6 +54,9 @@ int insertSymbol(const char* name, Type type, int kind);
    Returns: 1=found (*out set), 0=not found, -1=invalid args */
 int lookupSymbol(const char* name, Symbol** out);
 
+int typeEqual(Type A,Type B);
+
+int check_type_array_strong(Type A,Type B);
 #ifdef __cplusplus
 }
 #endif
